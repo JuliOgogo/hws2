063264a9c5e40a9e3a62fdfb761b180391abc514
Julia Titkova <juli.ogogo@yandex.ru>
2022-11-01 16:26:02 +0300

    HW4 (buttons css refactored)

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,9 +18,10 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        + (xType === 'red' ? ' ' + s.red :
-            xType === 'secondary' ? ' ' + s.secondary :
-                className ? ' ' + className : '') // задачка на смешивание классов
+        + (disabled ? ' ' + s.disabled :
+            xType === 'red' ? ' ' + s.red :
+                xType === 'secondary' ? ' ' + s.secondary :
+                    className ? ' ' + className : ' ' + s.default) // задачка на смешивание классов
 
     return (
         <button
