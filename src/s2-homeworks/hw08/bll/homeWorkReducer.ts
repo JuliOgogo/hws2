3beb05da8cacde8f1d0b6a9a5966f2d4eaacee3d
Julia Titkova <juli.ogogo@yandex.ru>
2022-11-18 14:27:59 +0300

    HW8 done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const stateCopy = [...state]
+            if (action.payload === 'up') {
+                return stateCopy.sort((a, b) => a.name > b.name ? 1 : -1)
+            }
+            if (action.payload === 'down') {
+                return stateCopy.sort((a, b) => a.name < b.name ? 1 : -1)
+            } else return stateCopy
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age > 18)
         }
         default:
             return state
