21a49ceac3392aea20dd7545bd94c221ada0b0e1
Julia Titkova <juli.ogogo@yandex.ru>
2022-11-21 23:24:51 +0300

    HW8 (sort refactored)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,12 +8,12 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
             const stateCopy = [...state]
-            if (action.payload === 'up') {
-                return stateCopy.sort((a, b) => a.name > b.name ? 1 : -1)
-            }
-            if (action.payload === 'down') {
-                return stateCopy.sort((a, b) => a.name < b.name ? 1 : -1)
-            } else return stateCopy
+            const sortedStateCopy = stateCopy.sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
+            return action.payload === 'up' ? sortedStateCopy : sortedStateCopy.reverse()
         }
         case 'check': {
             return state.filter(u => u.age > 18)
