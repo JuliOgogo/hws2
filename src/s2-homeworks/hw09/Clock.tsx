0af11144199e25c67d6aedf42bf688e7fd18e5cf
Julia Titkova <juli.ogogo@yandex.ru>
2022-11-22 14:49:56 +0300

    HW9 done

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -12,27 +12,45 @@ function Clock() {
     const start = () => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
+        stop()
+        const id: number = window.setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+        setTimerId(id)
     }
 
     const stop = () => {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
+        clearInterval(timerId)
+        setTimerId(undefined)
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
+        setShow(true)
     }
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
+        setShow(false)
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    /*let formatterForStringTime = new Intl.DateTimeFormat('ru', {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    })
+    let formatterForStringDate = new Intl.DateTimeFormat('ru', {
+        day: "numeric",
+        month: "numeric",
+        year: "numeric"
+    })
+    const stringTime = formatterForStringTime.format(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = formatterForStringDate.format(date) || <br/>*/ // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+    const stringTime = date.toLocaleTimeString() || <br/>
+    const stringDate = date.toLocaleDateString() || <br/>
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const stringDay = new Intl.DateTimeFormat('en', {weekday: "long"}).format(date) || <br/>
+    const stringMonth = new Intl.DateTimeFormat('en', {month: "long"}).format(date) || <br/>
 
     return (
         <div className={s.clock}>
@@ -66,14 +84,14 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={timerId !== undefined} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={timerId === undefined} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -3,10 +3,10 @@ import Clock from './Clock'
 import s2 from '../../s1-main/App.module.css'
 
 /*
-* 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave
-* 2 - в файле Clock.tsx из переменной date вычислить значения stringTime, stringDate, stringDay, stringMonth
-* 3 - в файле Clock.tsx дизэйблить кнопки стоп / старт если таймер не запущен / запущен соответственно
-* 4 - сделать стили в соответствии с дизайном
+* 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave +
+* 2 - в файле Clock.tsx из переменной date вычислить значения stringTime, stringDate, stringDay, stringMonth +
+* 3 - в файле Clock.tsx дизэйблить кнопки стоп / старт если таймер не запущен / запущен соответственно +
+* 4 - сделать стили в соответствии с дизайном +
 * */
 
 const HW9 = () => {
