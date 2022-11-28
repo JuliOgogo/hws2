7df70e811a7bbf448e14d47a0011bd5ec942e04c
Julia Titkova <juli.ogogo@yandex.ru>
2022-11-28 23:00:52 +0300

    HW12 done (before check)

---
+++ src/s2-homeworks/hw12/bll/themeReducer.test.ts
@@ -0,0 +1,16 @@
+import {changeThemeId, themeReducer} from "./themeReducer";
+
+let startState: { themeId: number }
+
+beforeEach(() => {
+    startState = {
+        themeId: 2
+    }
+})
+
+test('set correct theme id', () => {
+    const endState: typeof startState = themeReducer(startState, changeThemeId(3))
+
+    expect(endState.themeId).toBe(3)
+    expect(startState.themeId).toBe(2)
+})
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,22 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: changeThemeIdActionType): typeof initState => { // fixed
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID":
+            return {
+                ...state,
+                themeId: action.id
+            }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): changeThemeIdActionType => ({type: 'SET_THEME_ID', id}) // fixed
+
+export type changeThemeIdActionType = {
+    type: 'SET_THEME_ID',
+    id: number
+}
